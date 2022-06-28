6f2de3014f3cb8b5d0b04416888e54e54b027ff4
rbgreenway3rd <3rdgreenway@gmail.com>
2022-06-27 20:06:24 -0500

    full functionality for MusicPlayer is accomplished. All buttons work. README updated.

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # Music Share App
 
-This is a simple music sharing application built for practicing Material UI and Emotion styling. The application itself has minimal functionality. Therefore, the main purpose of this application is for practicing UI/UX design
+This is a music sharing application built for learning GraphQL and Apollo, as well as for practicing MaterialUI and React fundamentals.
 
-## NOTE:
+## Functionality
 
-As of React v.18, integrating parts of material ui have changed... particularly the usage of ThemeProvider, makeStyles, etc...
+SimpliToons is a music player application that allows a user to paste either a YouTube or SoundCloud URL into the search bar and, if the URL is valid and points to a playable song, the user will be prompted to fill out a form to provide the song's title and artist. This enables the user to save songs to their dashboard, enabling them to play a song just by clicking the play button. The user can also construct a queue from their saved songs. The app's music player will automatically begin playing the next song in the queue once the current song has finished playing.
 
-Currently, in order to make use of the "@mui/styles" library, one may have to include '--force' in their installation command due to npm throwing dependency errors.
+### Note on functionality:
+
+This application has semi-full CRUD functionality. A user can: CREATE data by saving songs, READ that data by having saved songs rendered to the dashboard, UPDATE the data by adding and/or removing songs from the current queue, and DELETE data by clicking the 'delete' button next to a song - removing it from either the queue or the dashboard entirely.
+However, due to how the database was constructed, multiple users cannot be created, therefore there is currently no functionality for creating a profile or logging in. This application 'remembers' some data through the use of the browser's localStorage.
+
+#### Tech-stack:
+
+JavaScript,
+ReactJs,
+Apollo,
+GraphQL,
+MaterialUI (using emotion),
+"subscriptions-transport-ws" (for WebSocket based subscriptions)
+
+see pacakge.json for all dependencies...
+NOTE: some dependencies listed may be deprecated as I was learning what to use and how as I built the applicaiton
--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -13,6 +13,7 @@ import { makeStyles } from "@mui/styles";
 import { songContext } from "../App";
 import { useQuery } from "@apollo/client";
 import { GET_QUEUED_SONGS } from "../graphql/queries";
+import ReactPlayer from "react-player";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -45,12 +46,65 @@ const useStyles = makeStyles((theme) => ({
 function MusicPlayer() {
   const { data, loading, error } = useQuery(GET_QUEUED_SONGS);
   const { state, dispatch } = React.useContext(songContext);
+  const [played, setPlayed] = React.useState(0);
+  const [seeking, setSeeking] = React.useState(false);
+  const [playedSeconds, setPlayedSeconds] = React.useState(0);
+  const [positionInQueue, setPositionInQueue] = React.useState(0);
+  const reactPlayerRef = React.useRef();
   const classes = useStyles();
+
+  // Enables MusicPlayer to begin playing next song in queue once current song ends
+  React.useEffect(() => {
+    const songIndex = data.queuedSongs.findIndex(
+      (song) => song.id === state.song.id
+    );
+    setPositionInQueue(songIndex);
+  }, [data.queuedSongs, state.song.id]);
+
+  // Updates the songs' positions in queue and stages upcoming song for player
+  React.useEffect(() => {
+    const nextSong = data.queuedSongs[positionInQueue + 1];
+    // Check for value slightly less than 1 due to 'step' being set to 0.01 in slider properties...
+    if (played >= 0.99 && nextSong) {
+      setPlayed(0);
+      dispatch({ type: "SET_SONG", payload: { song: nextSong } });
+    }
+  }, [data.queuedSongs, played, dispatch, positionInQueue]);
 
   function handleTogglePlay() {
     dispatch(state.isPlaying ? { type: "PAUSE_SONG" } : { type: "PLAY_SONG" });
   }
 
+  function handleSliderChange(event, newValue) {
+    setPlayed(newValue);
+  }
+
+  function handleSeekMouseDown() {
+    setSeeking(true);
+  }
+
+  function handleSeekMouseUp() {
+    setSeeking(false);
+    reactPlayerRef.current.seekTo(played);
+  }
+
+  function formatDuration(seconds) {
+    return new Date(seconds * 1000).toISOString().substring(11, 19);
+  }
+
+  function handlePlayPreviousSong() {
+    const previousSong = data.queuedSongs[positionInQueue - 1];
+    if (previousSong) {
+      dispatch({ type: "SET_SONG", payload: { song: previousSong } });
+    }
+  }
+
+  function handlePlayNextSong() {
+    const nextSong = data.queuedSongs[positionInQueue + 1];
+    if (nextSong) {
+      dispatch({ type: "SET_SONG", payload: { song: nextSong } });
+    }
+  }
   return (
     <>
       <Card variant="outlined" className={classes.container}>
@@ -64,7 +118,7 @@ function MusicPlayer() {
             </Typography>
           </CardContent>
           <div className={classes.controls}>
-            <IconButton>
+            <IconButton onClick={handlePlayPreviousSong}>
               <SkipPrevious />
             </IconButton>
             <IconButton onClick={handleTogglePlay}>
@@ -75,15 +129,38 @@ function MusicPlayer() {
                 <PlayArrow className={classes.playIcon} />
               )}
             </IconButton>
-            <IconButton>
+            <IconButton onClick={handlePlayNextSong}>
               <SkipNext />
             </IconButton>
             <Typography variant="subtitle1" component="p" color="textSecondary">
-              00:01:30
+              {formatDuration(playedSeconds)}
             </Typography>
           </div>
-          <Slider type="range" min={0} max={1} step={0.01} />
+          <Slider
+            value={played}
+            type="range"
+            min={0}
+            max={1}
+            step={0.01}
+            onChange={handleSliderChange}
+            onMouseDown={handleSeekMouseDown}
+            onMouseUp={handleSeekMouseUp}
+          />
         </div>
+        {/* the 'onProgress' property is a callback function from ReactPlayer
+        allowing user to interact with the music player's slider */}
+        <ReactPlayer
+          ref={reactPlayerRef}
+          onProgress={({ played, playedSeconds }) => {
+            if (!seeking) {
+              setPlayed(played);
+              setPlayedSeconds(playedSeconds);
+            }
+          }}
+          url={state.song.url}
+          playing={state.isPlaying}
+          hidden
+        />
         <CardMedia className={classes.thumbnail} image={state.song.thumbnail} />
       </Card>
       <QueuedSongList queuedSongs={data.queuedSongs} />
